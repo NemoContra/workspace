a7346aaf3f321cd1a4c0e866577b0b4090735e21
Florian Tischler <florian.tischler@gmx.net>
2019-05-09 17:16:36 +0200

    rxjs demo with typeahead

--- projects/rxjs-demo/src/app/app.component.ts
+++ projects/rxjs-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { animationFrameScheduler, of } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
+    of(42).pipe(
+      observeOn(animationFrameScheduler)
+    ).subscribe(() => console.log('0 Observable'));
+
+    requestAnimationFrame(() => {
+      console.log('1 animation frame');
+    });
+
+    setTimeout(() => {
+      console.log('2 setTimeout');
+    }, 0);
+
+    Promise.resolve().then(() => {
+      console.log('3 Promise');
+    });
+
+    console.log('4 Normales Console');
   }
 }
